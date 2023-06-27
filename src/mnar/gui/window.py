3bf2020926cd50f98f285d519a8ee3a8c5d699aa
David Scholberg <recombinant.vector@gmail.com>
2023-06-27 15:21:38 -0400

    removed fixed size of main window

--- src/mnar/gui/window.py
+++ src/mnar/gui/window.py
@@ -7,5 +7,4 @@ class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
         self.setWindowTitle("Mnar")
-        self.setFixedSize(QSize(400, 300))
         self.setCentralWidget(Editor())
