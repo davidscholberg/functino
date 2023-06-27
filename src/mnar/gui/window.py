efedfed31862e4eb5aff4b1340804dbec9e22e6b
David Scholberg <recombinant.vector@gmail.com>
2023-06-27 16:13:50 -0400

    modularized widget code

--- src/mnar/gui/window.py
+++ src/mnar/gui/window.py
@@ -1,18 +1,27 @@
-from PyQt6.QtCore import QSize
+from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QLabel, QMainWindow, QPlainTextEdit, QVBoxLayout, QWidget
 
 from mnar.gui.editor import Editor
+
+class CentralWidget(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+        self.setLayout(MainLayout())
+
+class MainLayout(QVBoxLayout):
+    def __init__(self) -> None:
+        super().__init__()
+        self.addWidget(Editor())
+        self.addWidget(QLabel("Output:"))
+        self.addWidget(OutputWidget())
 
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
         self.setWindowTitle("Mnar")
-        layout = QVBoxLayout()
-        layout.addWidget(Editor())
-        layout.addWidget(QLabel("Output:"))
-        output_widget = QPlainTextEdit()
-        output_widget.setReadOnly(True)
-        layout.addWidget(output_widget)
-        central_widget = QWidget()
-        central_widget.setLayout(layout)
-        self.setCentralWidget(central_widget)
+        self.setCentralWidget(CentralWidget())
+
+class OutputWidget(QPlainTextEdit):
+    def __init__(self) -> None:
+        super().__init__()
+        self.setReadOnly(True)
