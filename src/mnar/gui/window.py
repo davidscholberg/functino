22b05e4ae9973591270827f21dbc4c9612617869
David Scholberg <recombinant.vector@gmail.com>
2023-06-27 15:45:08 -0400

    added basic layout

--- src/mnar/gui/window.py
+++ src/mnar/gui/window.py
@@ -1,5 +1,5 @@
 from PyQt6.QtCore import QSize
-from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtWidgets import QLabel, QMainWindow, QPlainTextEdit, QVBoxLayout, QWidget
 
 from mnar.gui.editor import Editor
 
@@ -7,4 +7,12 @@ class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
         self.setWindowTitle("Mnar")
-        self.setCentralWidget(Editor())
+        layout = QVBoxLayout()
+        layout.addWidget(Editor())
+        layout.addWidget(QLabel("Output:"))
+        output_widget = QPlainTextEdit()
+        output_widget.setReadOnly(True)
+        layout.addWidget(output_widget)
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
