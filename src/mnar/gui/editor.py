9e7736af6e8e7b4af28569f73b3499df088afd96
David Scholberg <recombinant.vector@gmail.com>
2023-07-04 16:23:29 -0400

    set line number margin to be wide enough for two characters

--- src/mnar/gui/editor.py
+++ src/mnar/gui/editor.py
@@ -1,3 +1,4 @@
+from PyQt6 import sip
 from PyQt6.Qsci import QsciScintilla
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QFont, QPalette
@@ -7,9 +8,11 @@ class Editor(QsciScintilla):
     """Editor widget."""
     def __init__(self, parent: QWidget | None = None) -> None:
         super().__init__(parent)
-        self.setMarginLineNumbers(1, True)
         self.setFrameStyle(QFrame.Shape.NoFrame)
+        self.setFont(QFont("Consolas", 12))
         self.setCaretForegroundColor(self.palette().color(QPalette.ColorRole.Text))
         self.setMarginsBackgroundColor(self.palette().color(QPalette.ColorRole.Base))
         self.setMarginsForegroundColor(Qt.GlobalColor.darkGray)
-        self.setFont(QFont("Consolas", 12))
+        self.setMarginLineNumbers(0, True)
+        margin_width = self.SendScintilla(QsciScintilla.SCI_TEXTWIDTH, QsciScintilla.STYLE_LINENUMBER, sip.voidptr(b"0"))
+        self.setMarginWidth(0, round(margin_width * 2.5))
