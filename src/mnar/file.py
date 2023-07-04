87c904db0178564b889bb0fba5c39846a2efb08f
David Scholberg <recombinant.vector@gmail.com>
2023-07-04 17:00:32 -0400

    fixed issue where written files end up with double the lines on windows

--- src/mnar/file.py
+++ src/mnar/file.py
@@ -3,6 +3,6 @@ from tempfile import mkstemp
 def write_to_tmp_file(contents: str) -> str:
     """Write given contents to temporary file and return file path."""
     fd, path = mkstemp()
-    with open(fd, "w") as tmp_file:
+    with open(fd, "w", newline="") as tmp_file:
         tmp_file.write(contents)
     return path
