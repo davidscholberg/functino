b7c595655eb3d9b7babdb2ef3ae9e7f1aefa1273
David Scholberg <recombinant.vector@gmail.com>
2023-07-08 22:07:44 -0400

    partially fixed issue with updating line number width on new font
    There's currently a bug that only affects calls to the editor setFont
    function after the first one. The first call will set the correct line
    number margin width, but subsequent calls will set the width based on
    the previous font value instead of the current one. This may be due to a
    delay in the underlying scintilla library receiving the new font info.

--- src/mnar/gui/editor.py
+++ src/mnar/gui/editor.py
@@ -1,7 +1,7 @@
 from PyQt6 import sip
 from PyQt6.Qsci import QsciLexer, QsciScintilla
 from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QPalette
+from PyQt6.QtGui import QFont, QPalette
 from PyQt6.QtWidgets import QFrame, QWidget
 
 class Editor(QsciScintilla):
@@ -13,10 +13,23 @@ class Editor(QsciScintilla):
         self.setMarginsBackgroundColor(self.palette().color(QPalette.ColorRole.Base))
         self.setMarginsForegroundColor(Qt.GlobalColor.darkGray)
         self.setMarginLineNumbers(0, True)
-        margin_width = self.SendScintilla(QsciScintilla.SCI_TEXTWIDTH, QsciScintilla.STYLE_LINENUMBER, sip.voidptr(b"0"))
-        self.setMarginWidth(0, round(margin_width * 2.5))
         self.setMarginWidth(1, 0)
         self._lexer_copy = None
+
+    def setFont(self, f: QFont) -> None:
+        """
+        Reimplementation of setFont so that we can set a proper line number
+        margin width for the new font.
+
+        Note that there's currently a bug that only affects calls to this
+        function after the first one. The first call will set the correct line
+        number margin width, but subsequent calls will set the width based on
+        the previous font value instead of the current one. This may be due to a
+        delay in the underlying scintilla library receiving the new font info.
+        """
+        super().setFont(f)
+        margin_width = self.SendScintilla(QsciScintilla.SCI_TEXTWIDTH, QsciScintilla.STYLE_LINENUMBER, sip.voidptr(b"0"))
+        self.setMarginWidth(0, round(margin_width * 2.5))
 
     def setLexer(self, lexer: QsciLexer) -> None:
         """
