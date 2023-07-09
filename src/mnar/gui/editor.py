cbe74ea1feaa897bd8cdc9f266001dc663884983
David Scholberg <recombinant.vector@gmail.com>
2023-07-09 15:41:52 -0400

    fully fixed line number margin width getting updated on font changes

--- src/mnar/gui/editor.py
+++ src/mnar/gui/editor.py
@@ -1,6 +1,6 @@
 from PyQt6 import sip
 from PyQt6.Qsci import QsciLexer, QsciScintilla
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt, QTimer
 from PyQt6.QtGui import QFont, QPalette
 from PyQt6.QtWidgets import QFrame, QWidget
 
@@ -21,15 +21,15 @@ class Editor(QsciScintilla):
         Reimplementation of setFont so that we can set a proper line number
         margin width for the new font.
 
-        Note that there's currently a bug that only affects calls to this
-        function after the first one. The first call will set the correct line
-        number margin width, but subsequent calls will set the width based on
-        the previous font value instead of the current one. This may be due to a
-        delay in the underlying scintilla library receiving the new font info.
+        Note that this function does not set the new margin width right away;
+        instead, a one-shot timer with a small delay is used to do the update.
+        The reason for this is that scintilla is apparently not notified of font
+        changes right away, making the delay necessary. A simple sleep statement
+        doesn't seem to suffice since apparently the notification happens at
+        some other point in the event loop.
         """
         super().setFont(f)
-        margin_width = self.SendScintilla(QsciScintilla.SCI_TEXTWIDTH, QsciScintilla.STYLE_LINENUMBER, sip.voidptr(b"0"))
-        self.setMarginWidth(0, round(margin_width * 2.5))
+        QTimer.singleShot(10, self.reset_line_number_margin_width)
 
     def setLexer(self, lexer: QsciLexer) -> None:
         """
@@ -51,3 +51,8 @@ class Editor(QsciScintilla):
         does not return the lexer previously set with setLexer().
         """
         return self._lexer_copy
+
+    def reset_line_number_margin_width(self) -> None:
+        """Recalculate line number margin width based on current font size."""
+        margin_width = self.SendScintilla(QsciScintilla.SCI_TEXTWIDTH, QsciScintilla.STYLE_LINENUMBER, sip.voidptr(b"0"))
+        self.setMarginWidth(0, round(margin_width * 2.5))
