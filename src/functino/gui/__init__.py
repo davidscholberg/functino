db4ca02f7f4c81592332cb8c02ad75886d0b4c85
David Scholberg <recombinant.vector@gmail.com>
2023-07-12 19:31:25 -0400

    added proper handling for icon_set exceptions

--- src/functino/gui/__init__.py
+++ src/functino/gui/__init__.py
@@ -1,7 +1,10 @@
+from typing import cast
+
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QStyleHints
 from PyQt6.QtWidgets import QApplication
 
+from functino.gui.exception import pop_up_error_message
 from functino.gui.icon import IconSet
 from functino.gui.theme import Theme
 from functino.gui.window import MainWindow
@@ -15,13 +18,17 @@ def run() -> None:
     app.setStyle("fusion")
     theme = None
     icon_set = None
-    match app.styleHints().colorScheme():
-        case Qt.ColorScheme.Light:
-            theme = Theme(get_themes_path() / "light.xml")
-            icon_set = IconSet(IconSet.Light)
-        case _:
-            theme = Theme(get_themes_path() / "dark.xml")
-            icon_set = IconSet(IconSet.Dark)
-    main_window = MainWindow(theme, icon_set)
+    try:
+        match app.styleHints().colorScheme():
+            case Qt.ColorScheme.Light:
+                theme = Theme(get_themes_path() / "light.xml")
+                icon_set = IconSet(IconSet.Light)
+            case _:
+                theme = Theme(get_themes_path() / "dark.xml")
+                icon_set = IconSet(IconSet.Dark)
+    except Exception as e:
+        pop_up_error_message(e)
+        return
+    main_window = MainWindow(cast(Theme, theme), cast(IconSet, icon_set))
     main_window.show()
     app.exec()
